2b4cf3e92ba1c22369cd83d86028d29de24ac7b5
Dai Zhi Jie <zhijie.dai@hpe.com>
2023-07-10 17:34:59 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # privateDDNS
+
+## Introduction
+
+It is used for DDNS management in private network. The agent accepts the request from local network and update the DNS server(DNSPOD) if the ip address is changed.
+
+
+
+## Usage
+
+
+
+### Requirements
+
+```
+pip install -r requirements
+```
+
+
+
+### Run
+
+export the environment and run
+
+```shell
+export DNSPOD_TOKEN_ID = xxx
+export DNSPOD_TOKEN = 'xxx'
+export DOMAIN = 'xxx'
+export SUB_DOMAIN = 'xxx'
+```
+
+
+
+```shell
+python pddns.py
+```
+
+
+
+### Client Call
+
+Run the command at the client side periodically.
+
+```shell
+curl -X POST <server ip>:3053/hosts/<hostId>
+```
+
+
+
+
+
+## Docker Run
+
+create a env.txt with the following content
+
+```
+DNSPOD_TOKEN_ID = xxx
+DNSPOD_TOKEN = 'xxx'
+DOMAIN = 'xxx'
+SUB_DOMAIN = 'xxx'
+```
+
+start docker
+
+```
+docker run -d --net=host --env-file=env.txt darkmagician/privateDDNS
+```
+
---
+++ pddns.py
@@ -0,0 +1,122 @@
+from flask import request
+from flask import jsonify
+from flask import Flask
+import datetime
+import time
+from pyrate_limiter import Duration, RequestRate, Limiter, BucketFullException, Limiter
+from dnspod_sdk import DnspodClient
+import os
+
+
+def getENV(key, defaultVal=None):
+    if defaultVal:
+        return os.getenv(key, default=defaultVal)
+    val = os.getenv(key)
+    if val:
+        return val
+    raise Exception(f'env {key} is not configured')
+
+
+# Environments
+HTTP_PORT = getENV('HTTP_PORT', 3053)
+CACHE_EXP_IN_SEC = getENV('HTTP_PORT', 3600)
+DNSPOD_TOKEN_ID = getENV('DNSPOD_TOKEN_ID')
+DNSPOD_TOKEN = getENV('DNSPOD_TOKEN')
+DOMAIN = getENV('DOMAIN')
+SUB_DOMAIN = getENV('SUB_DOMAIN')
+
+# Global Variables
+dc = DnspodClient(DNSPOD_TOKEN_ID, DNSPOD_TOKEN, 'pddns')
+hosts_status = {}
+dns_cache = None
+cache_update_time = 0
+
+client_rate = Limiter(RequestRate(5, Duration.MINUTE))
+dns_rate = Limiter(RequestRate(100, Duration.HOUR))
+
+app = Flask(__name__)
+
+
+@app.route("/hosts/<hostId>", methods=["POST"])
+def register(hostId):
+    ip = request.remote_addr
+
+    try:
+        client_rate.try_acquire(ip)
+    except BucketFullException as err:
+        return jsonify({'error': f'The client [{ip}] reaches the rate limit.'}), 400
+    # print(f'==> Receive from {ip} for {hostId}')
+    try:
+        record = getDNSValue(hostId)
+        if record is None:
+            return jsonify({'error': f'unknown host {hostId}'}), 400
+        changed = updateDNS(hostId, ip, record)
+
+        hosts_status[hostId] = {'ip': ip, 'updatedTime': datetime.datetime.now()}
+        return jsonify({'ip': ip, 'changed': changed}), 200
+    except BucketFullException as err:
+        return jsonify({'error': f'The DNSAPI reaches the rate limit.'}), 400
+
+
+@app.route("/hosts", methods=["GET"])
+def hosts():
+    return jsonify(hosts_status), 200
+
+
+@app.route("/domains", methods=["GET"])
+def domains():
+    return jsonify(dns_cache), 200
+
+
+def updateDNS(hostId, ip, record):
+    oldIp = record['value']
+    if ip == oldIp:
+        return False
+
+    dns_rate.try_acquire('api')
+    name = getDNSName(hostId)
+    r = dc.post('/Record.Modify', data={'domain': DOMAIN, 'record_id': record['id'], 'record_type': 'A',
+                                        'sub_domain': name, 'record_line': '默认', 'value': ip})
+
+    print(r.json())
+    print(f'==> Update {name} from {oldIp} to {ip} ')
+    refreshDNSCache()
+    return True
+
+
+def refreshDNSCache():
+    global dns_cache
+    global cache_update_time
+    dns_rate.try_acquire('api')
+    print("Get Record.List")
+    r = dc.post('/Record.List', data={'domain': DOMAIN, 'record_type': 'A'})
+    result = r.json()
+    cache = {}
+    for r in result['records']:
+        key = r['name']
+        if key.endswith(SUB_DOMAIN):
+            cache[key] = r
+    dns_cache = cache
+    cache_update_time = time.time()
+
+
+def getDNSName(hostId):
+    return f'{hostId}.{SUB_DOMAIN}'
+
+
+def getDNSValue(host):
+    global dns_cache
+    global cache_update_time
+    print(f'=== {cache_update_time}')
+    if dns_cache is None or time.time() > cache_update_time + CACHE_EXP_IN_SEC:
+        refreshDNSCache()
+    record = dns_cache.get(getDNSName(host))
+    return record
+
+
+def main():
+    app.run(host="0.0.0.0", port=HTTP_PORT)
+
+
+if __name__ == '__main__':
+    main()
